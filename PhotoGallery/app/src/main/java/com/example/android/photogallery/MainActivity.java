855b9e95f7d3b48f6d13e67ae0f0de62bebcd30e
Ishan Agarwal <ishanagarwal.cs@gmail.com>
2019-01-19 23:21:23 -0600

    Commiting the internet connectivity functionality for Photo Gallery App.

--- PhotoGallery/app/src/main/java/com/example/android/photogallery/MainActivity.java
+++ PhotoGallery/app/src/main/java/com/example/android/photogallery/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.android.photogallery;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -31,19 +35,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                AlertDialog.Builder alertdialog = new AlertDialog.Builder(MainActivity.this);
-                alertdialog.setTitle("Choose a Keyword")
-                        .setItems(tempList.toArray(new CharSequence[tempList.size()]), new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                String key = tempList.get(i);
-                                searchWord.setText(key);
-                            }
-                        });
-                alertdialog.show();
+                if (isConnected()) {
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                    alertDialog.setTitle("Choose a Keyword")
+                            .setItems(tempList.toArray(new CharSequence[tempList.size()]), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    String key = tempList.get(i);
+                                    searchWord.setText(key);
+                                }
+                            });
+                    alertDialog.show();
+                } else
+                    Toast.makeText(MainActivity.this, "No Internet Connected", Toast.LENGTH_SHORT).show();
             }
-
         });
+    }
 
+    private boolean isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo == null || !networkInfo.isConnected()) {
+            return false;
+        }
+        return true;
     }
 }
+
+
